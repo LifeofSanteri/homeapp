5f8c752e34ad501584acf2c7030de4da9f9dd140
Santeri Savolainen <143079416+LifeofSanteri@users.noreply.github.com>
2023-12-11 15:22:13 +0200

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -20,7 +20,7 @@ function Login() {
     event.preventDefault();
     setErrors(Validation(values));
 
-    axios.post('https://home-app-o4a5.onrender.com/login', values)
+    axios.post('https://psqserver.onrender.com/login', values)
   .then((res) => {
     const status = res.data.status;
     const userId = res.data.userId;
